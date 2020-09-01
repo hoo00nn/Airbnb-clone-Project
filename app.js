8e4d7c787d2ae3ec9c1a41337211a4711622679a
hoo00nn <hoo00nn@naver.com>
2020-09-01 15:11:33 +0900

    Chore : body-parser install

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const users = require('./routes/users');
 const login = require('./routes/login');
 
 app.set('view engine', 'pug');
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}...`);
