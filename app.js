686f2bb3935118dc333b90ca757a21213303c1e1
hoo00nn <hoo00nn@naver.com>
2020-09-01 19:19:20 +0900

    Feat : login, logout, session, cookie 구현
    로그인시 쿠키와 세션 생성
    로그아웃시 쿠키 세션 삭제

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 3000;
 const users = require('./routes/users');
 const login = require('./routes/login');
@@ -8,6 +9,7 @@ const login = require('./routes/login');
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended : true}))
 
 app.listen(port, () => {
--- routes/login.js
+++ routes/login.js
@@ -2,24 +2,34 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
-const {userDB} = require('../public/database/db');
+const {userDB, sessionDB} = require('../public/database/db');
 
 router.get('/', (req, res) => {
-  res.render(path.join(__dirname, '../views/login.pug'));
+  sessionCheck(req.cookies)
+  .then(data => {
+    if (data.length !== 0) {
+      return res.render(path.join(__dirname, '../views/login.pug'), {id : data[0].id});
+    }
+    return res.render(path.join(__dirname, '../views/login.pug',));
+  });
 });
 
-router.post('/form', (req, res) => {
-  console.log(req.body);
-
+router.post('/loginCheck', (req, res) => {
   findUser(req.body)
   .then(data => {
-    if (data.length !== 0) return res.json({result : "true"});
-    return res.json({result : "false"});
+    if (data.length !== 0) {
+      let hash = encrypt();
+      res.cookie('SID', hash);
+      setSID(hash, req.body);
+    }
+    return res.redirect('/login');
   });
 });
 
 router.post('/logout', (req, res) => {
-
+  removeSession(req.cookies.SID);
+  res.clearCookie('SID');
+  return res.redirect('/login');
 });
 
 const findUser = function(info) {
@@ -29,6 +39,28 @@ const findUser = function(info) {
       resolve(docs);
     });
   })
+}
+
+const sessionCheck = function(cookie) {
+  return new Promise((resolve, reject) => {
+    sessionDB.find({SID : cookie.SID}, (err, docs) => {
+      if (err) resolve(false);
+      resolve(docs);
+    })
+  })
+}
+
+const setSID = function(hash, info) {
+  sessionDB.insert({SID : hash, id : info.id});
+}
+
+const removeSession = function(SID) {
+  sessionDB.remove({SID : SID}, {}, (err, removed) => {});
+}
+
+const encrypt = function() {
+  const CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  return Array(50).join().split(',').map(function() { return CHAR.charAt(Math.floor(Math.random() * CHAR.length));}).join('');
 }
 
 module.exports = router;
