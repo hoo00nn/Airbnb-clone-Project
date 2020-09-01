10f6c0bb0b6493f4779e991c94258bab6b0db55e
hoo00nn <hoo00nn@naver.com>
2020-09-01 12:27:39 +0900

    Chore : delete login routing
    로그인을 따로 관리하기 위해 라우팅 설정 살제

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,4 @@ const app = express();
 const router = express.Router();
 const path = require('path');
 
-router.get('/login', (req, res) => {
-  res.render(path.join(__dirname, '../views/login.pug'));
-})
-
 module.exports = router;
