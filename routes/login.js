4e6c79182df5c73f7fb772c2549da59f43273aef
hoo00nn <hoo00nn@naver.com>
2020-09-01 15:00:27 +0900

    feat : add login
    로그인 기능 추가

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,34 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
+const {userDB} = require('../public/database/db');
 
 router.get('/', (req, res) => {
   res.render(path.join(__dirname, '../views/login.pug'));
 })
+
+router.post('/form', (req, res) => {
+  console.log(req.body);
+
+  findUser(req.body)
+  .then(data => {
+    console.log(data);
+    if (data.length !== 0) return res.json({result : "true"});
+    return res.json({result : "false"});
+  });
+});
+
+router.post('/logout', (req, res) => {
+
+});
+
+const findUser = function(info) {
+  return new Promise((resolve, reject) => {
+    userDB.find({id : info.id, pw : info.pw}, (err, docs) => {
+      if (err) resolve(false);
+      resolve(docs);
+    });
+  })
+}
 
 module.exports = router;
