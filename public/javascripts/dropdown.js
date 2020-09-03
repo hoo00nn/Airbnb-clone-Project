ff9defe5de7f7a6f7016b226ca21978890b440f5
hoo00nn <hoo00nn@naver.com>
2020-09-03 17:08:13 +0900

    Chore : 프로젝트 구조 변경

--- public/javascripts/dropdown.js
+++
@@ -1,6 +0,0 @@
-let dropdown = document.querySelector('.dropdown');
-
-dropdown.addEventListener('click', () => {
-  if (dropdown.classList.contains('closed')) dropdown.classList.remove('closed');
-  else dropdown.classList.add('closed');
-});
