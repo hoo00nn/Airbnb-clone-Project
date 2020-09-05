e449e7cb4728584e080fee62dd8bbec9d3ef6ec5
hoo00nn <hoo00nn@naver.com>
2020-09-05 16:39:25 +0900

    Test : heroku nedb 테스트
    heroku 배포 시 nedb 정보 잘 불러와 지는지 확인

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -2,7 +2,9 @@ const Datastore = require('nedb')
 const path = require('path');
 let userDB = new Datastore();
 let sessionDB = new Datastore();
-let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms'), autoload : true});
+let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms')});
+
+roomsDB.loadDatabase();
 
 module.exports = {
   userDB,
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,9 @@ const login = require('./login');
 const logout = require('./logout');
 const signup = require('./signup');
 
+const test = require('./test');
+router.use('/test', test);
+
 router.use('/login', login);
 router.use('/logout', logout);
 router.use('/signup', signup);
