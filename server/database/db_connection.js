01d389c493085d1cb114b45168dbc7aa7fc88dec
hoo00nn <hoo00nn@naver.com>
2020-09-05 16:32:08 +0900

    Feat : 숙소 정보 더미 데이터 추가

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -1,8 +1,11 @@
-let Datastore = require('nedb')
+const Datastore = require('nedb')
+const path = require('path');
 let userDB = new Datastore();
 let sessionDB = new Datastore();
+let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms'), autoload : true});
 
 module.exports = {
   userDB,
-  sessionDB
+  sessionDB,
+  roomsDB
 };
