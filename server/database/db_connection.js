f0b5cfacb93475c5956321c79726fa9f668106ff
hoo00nn <hoo00nn@naver.com>
2020-09-05 16:55:42 +0900

    Feat : nedb 데이터 저장 방식 변경
    인 메모리 형식에서 파일 시스템 형식으로 변경

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -1,8 +1,12 @@
 const Datastore = require('nedb')
 const path = require('path');
-let userDB = new Datastore();
-let sessionDB = new Datastore();
-let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms'), autoload : true});
+let userDB = new Datastore({filename : path.join(__dirname, 'users')});
+let sessionDB = new Datastore({filename : path.join(__dirname, 'sessions')});
+let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms')});
+
+roomsDB.loadDatabase();
+userDB.loadDatabase();
+sessionDB.loadDatabase();
 
 module.exports = {
   userDB,
