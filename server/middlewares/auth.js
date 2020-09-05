cdad753345de9e7dbb31e0dad5fa94c3e7e34076
hoo00nn <hoo00nn@naver.com>
2020-09-05 13:10:10 +0900

    Refactor : 로그인 인증 미들웨어 추가
    코드의 가독성을 높이기 위해 로그인 인증기능을 미들웨어로 따로 처리

---
+++ server/middlewares/auth.js
@@ -0,0 +1,54 @@
+const {userDB} = require('../database/db_connection'); 
+const bcrypt = require('bcrypt');
+const Session = require('../model/session');
+const session = new Session();
+
+const getPassword = (email) => {
+  return new Promise(resolve => {
+    userDB.find({email}, (err, result) => {
+      resolve(result[0].password);
+    });
+  })
+}
+
+const checkPassword = (password, hash) => {
+  return new Promise(resolve => {
+    bcrypt.compare(password, hash, (err, isMatch) => {
+      resolve(isMatch);
+    })
+  })
+}
+
+const checkEmail = (email) => {
+  return new Promise(resolve => {
+    userDB.find({email : email}, (err, result) => {
+      if (result.length > 0) resolve(true)
+      else resolve(false)
+    })
+  })
+}
+
+const loginAuth = async (req, res, next) => {
+  const correctEmail = await checkEmail(req.body.email);
+  
+  if (correctEmail) {
+    const hashPassword = await getPassword(req.body.email);
+    const correctPassword = await checkPassword(req.body.password, hashPassword);
+    
+    if (correctPassword) {
+      const SID = session.encrypt();
+
+      res.cookie('SID', SID);
+      session.setSID(SID, req.body);
+    }
+
+    next();
+  }
+  else {
+    next();
+  }
+}
+
+module.exports = { 
+  loginAuth
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
-const path = require('path');
-const {userDB, sessionDB} = require('../database/db_connection');
-const bcrypt = require('bcrypt');
 const Session = require('../model/session');
 let session = new Session();
+const { loginAuth } = require('../middlewares/auth');
 
-router.post('/loginCheck', async (req, res) => {
-  const correctEmail = await checkEmail(req.body.email);
-  
-  if (correctEmail) {
-    const hashPassword = await getPassword(req.body.email);
-    const correctPassword = await checkPassword(req.body.password, hashPassword);
-    
-    if (correctPassword) {
-      const SID = session.encrypt();
 
-      res.cookie('SID', SID);
-      session.setSID(SID, req.body);
-    }
-
-    return res.redirect('/');
-  }
-  else {
-    return res.redirect('/');
-  }
+router.post('/loginCheck', loginAuth, (req, res) => {
+  return res.redirect('/');
 });
 
 router.get('/logout', (req, res) => {
@@ -33,30 +14,5 @@ router.get('/logout', (req, res) => {
   res.clearCookie('SID');
   return res.redirect('/');
 });
-
-const getPassword = (email) => {
-  return new Promise(resolve => {
-    userDB.find({email}, (err, result) => {
-      resolve(result[0].password);
-    });
-  })
-}
-
-const checkPassword = (password, hash) => {
-  return new Promise(resolve => {
-    bcrypt.compare(password, hash, (err, isMatch) => {
-      resolve(isMatch);
-    })
-  })
-}
-
-const checkEmail = (email) => {
-  return new Promise(resolve => {
-    userDB.find({email : email}, (err, result) => {
-      if (result.length > 0) resolve(true)
-      else resolve(false)
-    })
-  })
-}
 
 module.exports = router;
