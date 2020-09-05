0cd74539bba12b184c1d99ef2d6912de3785ea1b
hoo00nn <hoo00nn@naver.com>
2020-09-05 18:57:04 +0900

    Feat: rooms.js 생성 및 라우팅 설정
    숙소 검색 기능을 위한 rooms js파일 생성

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const login = require('./login');
 const logout = require('./logout');
 const signup = require('./signup');
+const rooms = require('./rooms');
 
 router.use('/login', login);
 router.use('/logout', logout);
 router.use('/signup', signup);
+router.use('/rooms', rooms);
 
 module.exports = router;
