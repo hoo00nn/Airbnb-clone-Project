7e83bbf3ed7c64685344c90a7ec07b911dedbb9c
hoo00nn <hoo00nn@naver.com>
2020-09-06 00:02:55 +0900

    Style: rooms 페이지 css 추가

--- server/middlewares/rooms.js
+++ server/middlewares/rooms.js
@@ -3,7 +3,14 @@ const { getRooms } = require('../model/rooms');
 const getOptionRooms = async (req, res) => {
   const data = await getRooms(req.query);
   
-  return res.render('rooms', {data : data});
+  return res.render('rooms', {
+    data : data,
+    location : req.query.location,
+    personnel : req.query.personnel,
+    checkin : req.query.checkin,
+    checkout : req.query.checkout,
+    length : data.length
+  });
 }
 
 module.exports = {
