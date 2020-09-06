2e630ed3668069c13078669fccc1470fc25f85dc
hoo00nn <hoo00nn@naver.com>
2020-09-06 16:22:29 +0900

    Style: 비밀번호 보기 기능 추가

--- client/public/javascripts/layout/header.js
+++ client/public/javascripts/layout/header.js
@@ -1,5 +1,6 @@
 const headerLogin = document.querySelector('#login');
 const headerRegister = document.querySelector('#register');
+const lookPassword = document.querySelector('.look__password');
 
 headerLogin.addEventListener('click', (e) => {
   document.querySelector('.header__register')
@@ -23,4 +24,11 @@ headerRegister.addEventListener('click', (e) => {
   .querySelector('.modal')
   .classList
   .remove('hidden');
+});
+
+lookPassword.addEventListener('click', () => {
+  const input = document.querySelector('.login__password').children.namedItem('password');
+  
+  if (input.getAttribute('type') === 'password') input.setAttribute('type', 'text');
+  else input.setAttribute('type', 'password');
 })
