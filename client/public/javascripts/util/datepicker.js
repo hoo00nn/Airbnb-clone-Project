d8e6edab98dc4c085b359dbd77bdc21d6fe5b86c
hoo00nn <hoo00nn@naver.com>
2020-09-09 17:37:50 +0900

    Feat: makeDaysElement 함수 추가
    한달의 일 수를 뷰에 나타내기 위해 DOM을  조작하여 나타내는 함수 추가

--- client/public/javascripts/util/datepicker.js
+++ client/public/javascripts/util/datepicker.js
@@ -21,4 +21,24 @@ class Calendar {
       days.insertAdjacentHTML('beforeend', `<li>${this.week[i]}</li>`);
     }
   }
+
+  makeDaysElement() {
+    const dayOfWeek = this.datepicker.querySelector('.dayOfWeek');
+    const firstDayOfMonth = new Date(`${this.year}-${this.month}-01`).getDay();
+    const totalDayOfMonth = new Date(this.year, this.month, 0).getDate();
+  
+    dayOfWeek.innerHTML = '';
+  
+    for(let i = 0; i < firstDayOfMonth; i++) {
+      dayOfWeek.insertAdjacentHTML('beforeend', `<li></li>`);
+    }
+  
+    for(let i = 1; i <= totalDayOfMonth; i++) {
+      const monthToString = this.month < 10 ? this.month.toString().padStart(2, '0') : this.month.toString();
+      const dayToString = i < 10 ? i.toString().padStart(2, '0') : i.toString();
+      const tag = `<li class="day" data-date=${this.year}-${monthToString}-${dayToString}>${i}</li>`;
+  
+      dayOfWeek.insertAdjacentHTML('beforeend', tag);
+    }
+  }
 }
