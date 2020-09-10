8150d9dd0abe7ebf3f01b29e4febace1da9337d3
hoo00nn <hoo00nn@naver.com>
2020-09-11 01:26:04 +0900

    Feat: reservation 데이터 스토어 생성
    예약 정보를 저장하기 위해 데이터 베이스 생성

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -3,10 +3,12 @@ const path = require('path');
 let userDB = new Datastore({filename : path.join(__dirname, 'users')});
 let sessionDB = new Datastore({filename : path.join(__dirname, 'sessions')});
 let roomsDB = new Datastore({filename : path.join(__dirname, 'rooms')});
+let reservationDB = new Datastore({filename : path.join(__dirname, 'reservation')});
 
 roomsDB.loadDatabase();
 userDB.loadDatabase();
 sessionDB.loadDatabase();
+reservationDB.loadDatabase();
 
 module.exports = {
   userDB,
