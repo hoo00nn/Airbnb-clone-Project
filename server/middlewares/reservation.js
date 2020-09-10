c4e252d1b9d6387de0eb589647759edf81cd3535
hoo00nn <hoo00nn@naver.com>
2020-09-11 02:04:45 +0900

    Feat: insertReservation 함수 구현
    예약 정보 DB에 저장하는 함수 구현

--- server/middlewares/reservation.js
+++ server/middlewares/reservation.js
@@ -3,6 +3,10 @@ const { reservationDB, sessionDB } = require('../database/db_connection');
 const reservation = async (req, res) => {
   if (Object.prototype.hasOwnProperty.call(req.cookies, 'SID')) {
     const email = await findEmail(req.cookies.SID);
+    req.body.email = email;
+    insertReservation(req.body);
+
+    res.render('reservation', data);
   }
   res.send('<script type="text/javascript">alert("로그인이 필요합니다. 로그인 하신 후 다시 시도해 주세요.");</script>"');
 }
@@ -14,6 +18,18 @@ const findEmail = (SID) => {
       resolve(data.email);
     })
   })
+}
+
+const insertReservation = (data) => {
+  const roomInfo = {
+    checkin : JSON.parse(data.data).checkin,
+    checkout : JSON.parse(data.data).checkout,
+    image : JSON.parse(data.data).image,
+    title : JSON.parse(data.data).title,
+    email : data.email
+  };
+
+  reservationDB.insert(ro);
 }
 
 module.exports = {
