e19d70fb567c1266d59d23e717bd481b8d95e06a
hoo00nn <hoo00nn@naver.com>
2020-09-11 02:14:10 +0900

    Refactor: 필요없는 파일 삭제

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const reservation = require('./reservaion');
-
-router.use('/reservation', reservation);
-
-module.exports = router;
