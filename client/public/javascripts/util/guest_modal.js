d688739a37a2daf3cc162eb51f002b4abaff0566
hoo00nn <hoo00nn@naver.com>
2020-09-10 16:14:34 +0900

    Feat: 게스트 모달 클릭 이벤트 등록
    모달 창에서 연령 별로 인원 수를 추가 삭제에 따라 동적으로 인원 수가 변경되고 input 박스 value값에 적용시키도록 수정

--- client/public/javascripts/util/guest_modal.js
+++ client/public/javascripts/util/guest_modal.js
@@ -6,3 +6,35 @@ guest_overlay.addEventListener('click', (e) => {
   
   e.target.parentNode.classList.add('hide');
 })
+
+guest.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const personnel = document.querySelector('#personnel');
+  let input_value = personnel.getAttribute('value') === null ? 0 : parseInt(personnel.getAttribute('value'));
+  let input = e.target.parentNode.querySelector('.count');
+
+  if (e.target.classList.contains('plus__button')) {
+    input.dataset.count = parseInt(input.dataset.count) + 1;
+    input.innerHTML = input.dataset.count;
+    personnel.setAttribute('value', input_value + 1);
+
+    if (input.dataset.count > 0) {
+      e.target.parentNode.querySelector('.minus__button').classList.remove('not__allow__click');
+      e.target.parentNode.querySelector('.minus__button').classList.add('allow__click');
+    }
+  }
+
+  if (e.target.classList.contains('minus__button')) {
+    input.dataset.count = parseInt(input.dataset.count) - 1;
+    input.innerHTML = input.dataset.count;
+    personnel.setAttribute('value', input_value - 1);
+
+    if (input.dataset.count < 1) {
+      e.target.parentNode.querySelector('.minus__button').classList.remove('allow__click');
+      e.target.parentNode.querySelector('.minus__button').classList.add('not__allow__click');
+    }
+  }
+})
+
+
