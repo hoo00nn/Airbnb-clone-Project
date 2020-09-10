92ee8ddf0722f8459958733b69840a708b54bfd3
hoo00nn <hoo00nn@naver.com>
2020-09-10 17:39:29 +0900

    Feat: checkin value 비교 로직 추가
    체크아웃 인풋 박스 클릭시 체크인 이전 날짜 선택 불가능하도록 체크인 인풋 박스 값을 비교해서 이전날짜는 클릭 못하도록 설정

--- client/public/javascripts/layout/searchbar.js
+++ client/public/javascripts/layout/searchbar.js
@@ -4,21 +4,33 @@ const personnel = document.querySelector('.personnel__wrap__left');
 
 checkin.addEventListener('click', (e) => {
   const target = e.target.offsetParent.querySelector('.datepicker__wrap');
-
-  if (target.classList.contains('hide')) target.classList.remove('hide');
-  else target.classList.add('hide');
+  hide(target);
 })
 
 checkout.addEventListener('click', (e) => {
   const target = e.target.offsetParent.querySelector('.datepicker__wrap');
+  hide(target);
 
-  if (target.classList.contains('hide')) target.classList.remove('hide');
-  else target.classList.add('hide');
+  const checkinValue = document.querySelector('#checkin').getAttribute('value');
+  const days = document.querySelector('.checkout').querySelectorAll('.day');
+  
+  days.forEach(v => {
+    if (checkinValue === null)  return 
+
+    const value = checkinValue === null ? 0 : parseInt(checkinValue.split('-').join(''));
+    const compareValue =  parseInt(((v.dataset.date)).split('-').join(''));
+    if (value - compareValue >= 0) {
+      v.classList.add('not__allow__click');
+    } 
+  });
 })
 
 personnel.addEventListener('click', (e) => {
   const target = e.currentTarget.parentNode.previousSibling;
+  hide(target);
+})
 
+const hide = (target) => {
   if (target.classList.contains('hide')) target.classList.remove('hide');
   else target.classList.add('hide');
-})
+}
