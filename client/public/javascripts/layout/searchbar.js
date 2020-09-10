bb97e17b027cc170c95c808771223c301ce8c02b
hoo00nn <hoo00nn@naver.com>
2020-09-10 16:09:57 +0900

    Feat: personnel 클릭 이벤트 등록
    인원 input박스를 클릭 시 모달이 보여지고 숨겨지도록 하기 위해 클릭 이벤트 등록

--- client/public/javascripts/layout/searchbar.js
+++ client/public/javascripts/layout/searchbar.js
@@ -1,5 +1,6 @@
 const checkin = document.querySelector('.checkin__wrap');
 const checkout = document.querySelector('.checkout__wrap');
+const personnel = document.querySelector('.personnel__wrap__left');
 
 checkin.addEventListener('click', (e) => {
   const target = e.target.offsetParent.querySelector('.datepicker__wrap');
@@ -10,6 +11,13 @@ checkin.addEventListener('click', (e) => {
 
 checkout.addEventListener('click', (e) => {
   const target = e.target.offsetParent.querySelector('.datepicker__wrap');
+
+  if (target.classList.contains('hide')) target.classList.remove('hide');
+  else target.classList.add('hide');
+})
+
+personnel.addEventListener('click', (e) => {
+  const target = e.currentTarget.parentNode.previousSibling;
 
   if (target.classList.contains('hide')) target.classList.remove('hide');
   else target.classList.add('hide');
