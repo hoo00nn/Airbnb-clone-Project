0cf878cb5a7e51506763fc259ae123fadfefe098
hoo00nn <hoo00nn@naver.com>
2020-09-11 03:13:28 +0900

    Feat: makePriceOfPeriod 함수 구현
    예약 모달에서 설정한 일 수에 해당하는 숙박비용을 조작하고 입력하기위해 querySelector를 이용하여 구현

--- client/public/javascripts/reservation_modal.js
+++ client/public/javascripts/reservation_modal.js
@@ -24,4 +24,18 @@ class Reservation {
   
     guest.setAttribute('value', `${this.personnel}명`);
   }
+
+  makePriceOfPeriod() {
+    const priceLeft = this.reservation.querySelector('.reservation__info').querySelector('.price__left');
+    const priceRight = this.reservation.querySelector('.reservation__info').querySelector('.price__right');
+    const start = parseInt(this.checkin.split('-')[this.checkin.split('-').length-1]);
+    const end = parseInt(this.checkout.split('-')[this.checkout.split('-').length-1]);
+    const period = end - start;
+    const price = this.makeCommaOfPrice(parseInt(this.price.split(',').join('')) * period);
+  
+    priceLeft.innerText = `₩${this.price} X ${period}박`
+    priceRight.innerText = `₩${price}`;
+  
+    return price;
+  }
 }
